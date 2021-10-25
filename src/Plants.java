912976a0b46dcf818b0eed23e590026311a174fb
Svyatoslav Zhigajlo <s.zhigajlo@gmail.com>
2021-10-25 17:10:33 +0300

    Создал класс Растения

---
+++ src/Plants.java
@@ -0,0 +1,6 @@
+public class Plants {
+    private String root; //корневая система
+    private String bloomColor; //цвет цветения
+    private int lifeTime; //время жизни
+    private int heightGrowth; //высота роста см
+}
