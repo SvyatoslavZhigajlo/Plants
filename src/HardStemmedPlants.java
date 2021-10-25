002bebc79fac42cbc6bfd5b61b783b0bfc9bd65a
Svyatoslav Zhigajlo <s.zhigajlo@gmail.com>
2021-10-25 17:14:48 +0300

    Создал класс РАСТЕНИЯ ТВЕРДОСТЕБЛЕВЫЕ

---
+++ src/HardStemmedPlants.java
@@ -0,0 +1,5 @@
+public class HardStemmedPlants extends Plants {
+    private int barrelDiameter; //диаметр ствола
+    private int numberStems; //количество стеблей из корня
+    private String branching; //способ ветвления
+}
